03a8a5429e1486a0a189d86763148d23ae7c3f25
Manzi Joseph <mjoseph@bluefin.com>
2026-03-17 18:11:01 +0000

    almost restarted again

--- main.go
+++ main.go
@@ -1,193 +1,49 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
-	"strconv"
-	"strings"
-	"time"
+	"math"
 )
 
 func main() {
+	var age int // variable declaration
+	fmt.Println("Enter your age: ", age)
+	age = 25 // variable assignment
+	fmt.Println("My age after assignment is:", age)
+	age = 54 // variable reassignment
+	fmt.Println("My age after reassignment is:", age)
 
-	// variables explicit type
-	var name string = "Manzi"
-	var age int = 90
+	var newAge int = 67
+	fmt.Println("my new age : ", newAge)
 
-	fmt.Println(" Name is " + name)
-	fmt.Println(" Age  is", age)
+	// multiple varaibles declaration
+	var price, quantity int = 5000, 100 // declaring multiple vairables
+	fmt.Println("price is ", price, "quantity is ", quantity)
 
-	fmt.Println()
-
-	// multiple variable at once
-	var c, d int = 10, 20
-	a, b := "hello", true
-
-	fmt.Println(" C Value ", c)
-	fmt.Println(" D Value ", d)
-	fmt.Println(" A Value ", a)
-	fmt.Println(" B Value ", b)
-
-	fmt.Println()
-
-	// block declaration
 	var (
-		host string = "localhost"
-		port int    = 8080
-		tls  bool   = true
+		anotherName = "Zeinab"
+		herAge      = 38
+		height      int
 	)
 
-	fmt.Println(" Host is " + host)
-	fmt.Println(" Port is ", port)
-	fmt.Println(" tls is ", tls)
+	fmt.Println("The Queens name is : ", anotherName)
+	fmt.Println("My age is: ", herAge)
+	fmt.Println("my height is ", height)
+
+	count := 10
+	fmt.Println("Count =", count)
+
+	//lets do more variable declaration
+	a, b := 20, 30 //declare variables a and b
+	fmt.Println("a is ", a, "b is ", b)
+	b, c := 50, 69 // b is already declared but c is new
+	fmt.Println("b is ", b, "c is ", c)
+	b, c = 90, 100 // assign new values to already assigned variables
+	fmt.Println("changed b is ", b, "c is ", c)
 
 	fmt.Println()
 
-	// constants
-	const MaxRetries = 3
-	const AppName = "PayConex"
-
-	// Typed constant
-	const Timeout time.Duration = 30 * time.Second
-
-	// Go implicit
-	var x int = 42
-	var z string = strconv.Itoa(x) //int -> string
-
-	fmt.Println(" Z Value ", z)
-	//fmt.Println(" Y Value ", y)
-
-	// okay lets put all these together
-
-	//----- 1. Basic declaration & Assignment
-
-	var firstName string = "Manzi"
-	lastname := "MJ"
-	var salary float64 = 1000.00
-
-	// Concatenation
-
-	fullName := firstName + " " + lastname
-	fmt.Println("Full Name is :", fullName)
-
-	// -----2. Updating / reassigning
-	age = age + 1  // birthday
-	salary *= 1.10 // 10% raise
-	fmt.Printf("Updated age is : age =%d, salary is =%.2f\n", age, salary)
-
-	//-----4 Type Conversation
-	var score int = 95
-	scoreFloat := float64(score)
-	scoreStr := strconv.Itoa(score)
-
-	fmt.Printf("int: %d | float: %.1f | string: \"%s\" and ", score, scoreFloat, scoreStr)
-
-	//----5. String Manipulation ----
-	role := " Certification Analyst II "
-	role = strings.TrimSpace(role)
-	role = strings.ToUpper(role)
-	fmt.Println("Role", role)
-
-	fmt.Println()
-
-	words := strings.Split(fullName, " ")
-	fmt.Println("Name parts: ", words) // [Manzi Joseph]
-	fmt.Println("First word: ", words[0])
-
-	fmt.Println()
-
-	// ----6. Zero Values at work
-	var counter int
-	var label string
-	var active bool
-
-	counter++ //0 --> without initializing
-	label = "EMV Cert"
-	active = !active // false --> true
-
-	fmt.Printf("counter=%d, label=%s, active=%v\n", counter, label, active)
-
-	// -----7. Constants vs Variable
-	attempts := 0
-
-	for attempts < MaxRetries {
-		attempts++
-		fmt.Printf("Attempts %d of %d\n", attempts, MaxRetries)
-	}
-
-	fmt.Println()
-	fmt.Println("The sum of two numbers", add(90, 80))
-	fmt.Println("Hello MR ", greet("Manzi Joseph"))
-	sayHi()
-
-	fmt.Println()
-	// Caller unpacks both values
-	result, err := divide(10, 2)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("The results of the division ", result) // 5
-
-	fmt.Println()
-
-	min, max := minMax([]int{4, 7, 2, 9, 1})
-	fmt.Println("Min:", min, "Max:", max)
-
-	// error creation
-	// simple error string
-	errors.New("Something went wrong")
-
-	// formatted error with context
-	var userID = "Manzi Joseph"
-	fmt.Errorf("user %d not found: %w", userID, err)
-
+	x, y := 145.4, 67.9
+	k := math.Min(x, y)
+	fmt.Println("Minimum value is ", k)
 }
-
-// okay lets work on the functions
-
-// addition
-func add(a int, b int) int {
-	return a + b
-}
-
-// named paramenter function
-func greet(name string) string {
-	return "Hello, " + name
-}
-
-// no parameter no return value
-func sayHi() {
-	fmt.Println("Hello Genius")
-}
-
-// returns a results and an error - Go's signature patterns
-func divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, errors.New("Division by zero")
-	}
-	return a / b, nil
-}
-
-// named returns
-func minMax(nums []int) (min, max int) {
-	min, max = nums[0], nums[0]
-	for _, v := range nums {
-		if v < min {
-			min = v
-		}
-		if v > max {
-			max = v
-		}
-	}
-
-	return //"naked returns - returns min and max"
-}
-
-// error is built in interface one method
-type error interface {
-	Error() string
-}
-
-// nil means "no error"
-var err error = nil
